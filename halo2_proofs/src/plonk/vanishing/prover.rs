37ea45f34c2ab3e9043fba74a2ba493c2a9efe96
CPerezz <c.perezbaro@gmail.com>
2023-03-06 12:57:31 +0100

    chore: Update merged main

--- halo2_proofs/src/plonk/vanishing/prover.rs
+++ halo2_proofs/src/plonk/vanishing/prover.rs
@@ -52,7 +52,7 @@ impl<C: CurveAffine> Argument<C> {
         let n_threads = current_num_threads();
         let n = 1usize << domain.k() as usize;
         let n_chunks = n_threads + if n % n_threads != 0 { 1 } else { 0 };
-        let mut rand_vec = vec![C::Scalar::zero(); n];
+        let mut rand_vec = vec![C::Scalar::ZERO; n];
 
         let mut thread_seeds: Vec<ChaCha20Rng> = (0..n_chunks)
             .into_iter()
