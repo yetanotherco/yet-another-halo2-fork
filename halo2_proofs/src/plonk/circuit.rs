f07d35f47f4fe2e3c7efa90101675a308ef8600c
ying tong <yingtong.lai@gmail.com>
2023-08-24 02:13:04 +0800

    Include shuffles in PinnedConstraintSystem fmt

--- halo2_proofs/src/plonk/circuit.rs
+++ halo2_proofs/src/plonk/circuit.rs
@@ -1623,6 +1623,7 @@ impl<'a, F: Field> std::fmt::Debug for PinnedConstraintSystem<'a, F> {
             .field("fixed_queries", self.fixed_queries)
             .field("permutation", self.permutation)
             .field("lookups", self.lookups)
+            .field("shuffles", self.shuffles)
             .field("constants", self.constants)
             .field("minimum_degree", self.minimum_degree);
         debug_struct.finish()
