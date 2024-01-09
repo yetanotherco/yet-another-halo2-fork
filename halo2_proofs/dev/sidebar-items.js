7d409f96d58e06fdd03ccd1426cc3790a01dde24
CPerezz <CPerezz@users.noreply.github.com>
2024-01-09 11:21:10 +0000

    deploy: 5b2f5f67d8d7376267eb84c190ecade74fc6f62f

--- halo2_proofs/dev/sidebar-items.js
+++ halo2_proofs/dev/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CellValue","FailureLocation","VerifyFailure"],"fn":["circuit_dot_graph"],"mod":["cost","cost_model","metadata"],"struct":["CircuitGates","CircuitLayout","MockProver","TracingFloorPlanner"]};
+window.SIDEBAR_ITEMS = {"enum":["CellValue","FailureLocation","InstanceValue","VerifyFailure"],"fn":["circuit_dot_graph"],"mod":["cost","cost_model","metadata"],"struct":["CircuitGates","CircuitLayout","MockProver","TracingFloorPlanner"]};
